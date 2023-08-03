b0c4c50ca21c029d9b19310d85b1aa95f42986d5
goku <goku@gokus-MacBook-Pro.local>
2023-08-03 19:15:07 +0530

    added footer

---
+++ src/components/Footer.js
@@ -0,0 +1,30 @@
+import logo_small from '../../public/logo_small.svg'
+import twitter_icon from '../../public/twitter_icon.svg'
+import Image from 'next/image';
+import Link from 'next/link';
+
+const Footer = () => {
+    return (
+        <footer className="flex items-center justify-center py-4 px-12 bg-gray-800 text-white space-x-2 sm:space-x-6 text-xs">
+            <Link href="/" className="hover:underline"> <Image
+                src={logo_small}  // replace with your actual logo path
+                width={50}
+                height={50}
+                alt="Logo"
+            /></Link>
+            <Link href="/terms-and-services" className="hover:underline" target="_blank">Terms & Services</Link>
+            <Link href="/privacy-policy" className="hover:underline" target="_blank">Privacy Policy</Link>
+            <Link href="/contact-us" className="hover:underline" target="_blank">Contact Us</Link>
+            <Link href="https://twitter.com/onlyhentaiapp" target="_blank" rel="noopener noreferrer">
+                <Image
+                    src={twitter_icon}  // replace with your actual logo path
+                    width={26}
+                    height={26}
+                    alt="Logo"
+                />
+            </Link>
+        </footer>
+    );
+};
+
+export default Footer;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import { Inter } from 'next/font/google'
 import Header from "../components/Header"
+import Footer from "@/components/Footer";
 import { AuthProvider } from '../context/AuthContext'
 import { GlobalProvider } from "@/context/GlobalContext";
 
@@ -15,6 +16,7 @@ export default function App({ Component, pageProps }) {
         <div className="mt-20 p-20 min-h-90vh">
           <Component {...pageProps} />
         </div>
+        <Footer />
       </GlobalProvider>
     </AuthProvider>
   )
