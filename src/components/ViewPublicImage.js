e9114aecf41466eced0a2dcf5e7e91d426525d2f
goku <goku@gokus-MacBook-Pro.local>
2023-08-07 22:46:30 +0530

    Updated view public image to include copy data

--- src/components/ViewPublicImage.js
+++ src/components/ViewPublicImage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useState } from 'react';
 import { userAuth } from '@/context/AuthContext';
 import { TrashIcon, DownloadIcon, CloseIcon } from "@/components/Icons";
 import axios from 'axios';
@@ -11,13 +11,18 @@ const baseImgLink = `${process.env.API_BASE_URL}/generations`;
 export default function ViewPublicImage({ onClose }) {
     const { user } = userAuth();
     const { selectedPublicImage, setPublicImages } = useGlobalContext();
+    const [prompt, setPrompt] = useState(selectedPublicImage.prompt);
+    const [negativePrompt, setNegativePrompt] = useState(selectedPublicImage.parameters.negative_prompt);
+    const [guidanceScale, setGuidanceScale] = useState(selectedPublicImage.parameters.guidance_scale);
+    const [seed, setSeed] = useState(selectedPublicImage.parameters.seed);
     const idToken = user ? user.accessToken : null;
 
-    const handleGenerate = () => {
-        console.log('generating from public image');
-        onClose();
-    };
+    const handleCopy = () => {
+        //this should copy the prompt and negative prompt to the clipboard
+        navigator.clipboard.writeText(`Prompt:\n${prompt}\n\nNegative Prompt:\n${negativePrompt} \n\nGuidance Scale: ${guidanceScale} \n\nSeed: ${seed}`);
+    }
 
+    const inputClasses = 'w-full px-2 py-1 border text-gray-800 focus:outline-none focus:ring-2 focus:ring-primary rounded-md';
 
     return (
         <div className="fixed inset-0 flex items-center justify-center z-20 bg-black bg-opacity-60 px-20 pt-20 pb-4 md:pb-10">
@@ -38,10 +43,24 @@ export default function ViewPublicImage({ onClose }) {
                                 blurDataURL={blurImage}
                             />
                         </div>
-                        <div className='flex flex-col mt-4 space-y-4 grow'>
-                            <textarea rows={4} type="text" defaultValue={selectedPublicImage.prompt} className="w-full px-2 py-1 mt-4 border text-gray-800 focus:outline-none focus:ring-2 focus:ring-primary rounded-md" />
+                        <div className='flex flex-col mt-4 space-y-4 grow text-sm text-gray-900'>
+                            <label className="">Prompt</label>
+                            <textarea rows={4} type="text" defaultValue={prompt} onChange={(e) => setPrompt(e.target.value)} className={inputClasses} />
+                            <label className="">Negative Prompt</label>
+                            <textarea rows={4} type="text" defaultValue={negativePrompt} onChange={(e) => setNegativePrompt(e.target.value)} className={inputClasses} />
+                            <div className='flex flex-row space-x-2 '>
+                                <div className='flex flex-col space-y-1'>
+                                    <label className=''>Guidance Scale</label>
+                                    <input type="number" defaultValue={guidanceScale} onChange={(e) => setGuidanceScale(e.target.value)} className={inputClasses} />
+                                </div>
+                                <div className='flex flex-col space-y-1'>
+                                    <label className=''>Seed</label>
+                                    <input type="number" defaultValue={seed} onChange={(e) => setSeed(e.target.value)} className={inputClasses} />
+                                </div>
+                            </div>
                             <div className='flex flex-row space-x-2 text-sm'>
-                                <button onClick={handleGenerate} className="px-4 py-2 text-sm font-bold text-white bg-primary rounded-md border border-primary hover:bg-primaryDark">Generate Similar</button>
+                                <button onClick={handleCopy} className="px-4 py-2 text-sm font-bold text-white bg-purple-500 rounded-md border border-purple-500 hover:bg-purple-700">Copy Data</button>
+
                             </div>
 
                         </div>
