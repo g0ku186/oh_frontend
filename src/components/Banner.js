0f16f39755d1d0c4ecb9dfba442b39a0d7f77a1f
goku <goku@gokus-MacBook-Pro.local>
2023-09-11 15:34:28 +0530

    added year

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -8,7 +8,7 @@ export default function Banner() {
         <>
             {show && (<div className="flex items-center gap-x-6 bg-red-600 px-6 py-2.5 sm:px-3.5 sm:before:flex-1 fixed w-full right-0 bottom-0">
                 <p className="text-sm leading-6 text-white">
-                    Service Notice: We are shutting down our services starting Sept 20th. If you have any queries please send an email to onlyhentaiteam@gmail.com
+                    Service Notice: We are shutting down our services starting Sept 20th, 2023. If you have any queries please send an email to onlyhentaiteam@gmail.com
                 </p>
                 <div className="flex flex-1 justify-end">
                     <button type="button" className="-m-3 p-3 focus-visible:outline-offset-[-4px]" onClick={() => setShow(false)}>
